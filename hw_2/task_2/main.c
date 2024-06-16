727eb8e109c0db7517015d1d0e16c84a19b9a350
Kilill_Shugay <shugaykirilll@gmail.com>
2024-06-16 17:33:37 +0200

    hw_2

---
+++ hw_2/task_2/main.c
@@ -0,0 +1,78 @@
+#include "stm32c0xx_hal.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define LED_PORT GPIOA
+#define RED_PIN GPIO_PIN_0
+#define YELLOW_PIN GPIO_PIN_1
+#define GREEN_PIN GPIO_PIN_4
+
+#define BUTTON_PORT GPIOA
+#define BUTTON_PIN GPIO_PIN_13
+
+#define LED_PORT_CLK_ENABLE __HAL_RCC_GPIOB_CLK_ENABLE
+#define VCP_TX_Pin GPIO_PIN_2
+#define VCP_RX_Pin GPIO_PIN_15
+
+void initGPIO(int led)
+{
+    GPIO_InitTypeDef GPIO_Config;
+
+    GPIO_Config.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_Config.Pull = GPIO_NOPULL;
+    GPIO_Config.Speed = GPIO_SPEED_FREQ_HIGH;
+
+    GPIO_Config.Pin = led;
+
+    LED_PORT_CLK_ENABLE();
+    HAL_GPIO_Init(LED_PORT, &GPIO_Config);
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+}
+
+void SwitchLight(int led, int time)
+{
+    HAL_GPIO_WritePin(LED_PORT, led, GPIO_PIN_SET);
+    HAL_Delay(time);
+    HAL_GPIO_WritePin(LED_PORT, led, GPIO_PIN_RESET);
+}
+
+void flashingLight(int led, int time)
+{
+    for (int i = 1; i < 4; i++)
+    {
+        HAL_Delay(time);
+        HAL_GPIO_WritePin(LED_PORT, led, GPIO_PIN_SET);
+        HAL_Delay(time);
+        HAL_GPIO_WritePin(LED_PORT, led, GPIO_PIN_RESET);
+    }
+}
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();
+
+    initGPIO(RED_PIN);
+    initGPIO(YELLOW_PIN);
+    initGPIO(GREEN_PIN);
+
+    while (1)
+    {
+        HAL_GPIO_WritePin(LED_PORT, RED_PIN, GPIO_PIN_SET);
+        int status;
+        if ((status = HAL_GPIO_ReadPin(BUTTON_PORT, BUTTON_PIN)) == 1)
+        {
+            HAL_Delay(3000);
+            flashingLight(RED_PIN, 1000);
+            SwitchLight(YELLOW_PIN, 3000);
+            SwitchLight(GREEN_PIN, 5000);
+            flashingLight(GREEN_PIN, 1000);
+            SwitchLight(YELLOW_PIN, 3000);
+            status = 0;
+        }
+    };
+
+    return 0;
+}
